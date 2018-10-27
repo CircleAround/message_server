da8e92223661a2e45d3159c28830d15819764d03
ted <ted@circlearound.co.jp>
2018-10-27 17:35:54 +0900

    画像保存のルーティング修正 API

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :messages, only: [:index, :show, :create, :update, :destroy] do
-      post 'images', to: 'attach_image', on: :member
+      post 'images', to: 'messages#attach_image', on: :member
     end
 
     post :sign_up, to: "registrations#sign_up"
